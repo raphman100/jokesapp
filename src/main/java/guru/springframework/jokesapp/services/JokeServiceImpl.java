fa92bd8764cb531a0da62c60bda86fac82bbb9d0
Unknown <raphman100@gmail.com>
2020-04-11 11:22:41 -0400

    Added Bean Config for outside dependency

--- src/main/java/guru/springframework/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/jokesapp/services/JokeServiceImpl.java
@@ -8,7 +8,7 @@ public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-	public JokeServiceImpl() {
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
 		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
 	}
 
